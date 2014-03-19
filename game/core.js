55855652c0212b7fcba035418564c0ee41afe41c
Nikita Zubarets <zubarets@gmail.com>
2014-03-19 23:47:16 +0200

    Append user action methods

--- game/core.js
+++ game/core.js
@@ -1,4 +1,5 @@
 var Core = new function() {
+	this.users = [];
 	this.onProblemGenerated = [];
 	this.runProblemGeneratedHandlers = function(){
 		for(var i=0; i<onProblemGenerated.length; i++){
@@ -13,6 +14,39 @@ var Core = new function() {
 		var solution = a + b;
 
 		return {a: a, b: b, fake: [fakeA, fakeB, solution], solution: solution };
+	}
+	this.setUser = function(user) {
+		if(user) {
+			this.users.push(user);
+			return user
+		} else {
+			throw new Error('You need pass a user object!');
+		}
+	}
+	this.getUserById = function(userID) {
+		if(userID) {
+			return this.users[userID];
+		} else {
+			throw new Error('You need pass a userID!');
+		}
+	}
+	this.getUsers = function() {
+		return this.users;
+	}
+	this.removeUserById = function(userID) {
+		if(userID) {
+			delete this.users[userID];
+			
+			return 'The user with id ' + userID + ' was deleted';
+		} else {
+			throw new Error('You need pass a userID!');
+		}
+	}
+	this.saveUsers = function() {
+		sessionStorage.setItem('users', this.users);
+	}
+	this.getStoredUsers = function() {
+		$.parseJSON( sessionStorage.getItem('users') );
 	}
 };
 
