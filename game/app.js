c05148ab4e1866a3559fedaac2db4d372e9df585
zubarets <zubarets@gmail.com>
2014-03-20 10:39:11 -0400

    Some code refactoring

--- game/app.js
+++ game/app.js
@@ -1,45 +1,50 @@
-var app = require('express')()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server),
-  core = require('./core');
+var app = require('express')(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server),
+  	Core = require('./model/core'),
+  	User = require('./model/user');
 
 server.listen(8054);
 
 //Add files
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  	res.sendfile(__dirname + '/view/index.html');
 });
-
-app.get('/index.js', function (req, res) {
-  res.sendfile(__dirname + '/index.js');
-});
-
 app.get('/style.css', function (req, res) {
-  res.sendfile(__dirname + '/style.css');
+  	res.sendfile(__dirname + '/view/style.css');
+});
+app.get('/index.js', function (req, res) {
+  	res.sendfile(__dirname + '/view/index.js');
+});
+app.get('/solver.js', function (req, res) {
+  	res.sendfile(__dirname + '/view/solver.js');
+});
+app.get('/user-action.js', function (req, res) {
+  	res.sendfile(__dirname + '/view/user-action.js');
 });
 
 io.sockets.on('connection', function (socket) {
 
-	socket.on('genNewNum', function(user) {
-		socket.emit('numberGen', core.problem());
+	socket.on('genNewNum', function() {
+		socket.emit('numberGen', Core.problem());
 	});
 	
 	//User's action
 	socket.on('saveUser', function(user) {
-		socket.emit('savedUser', core.setUser(user));
-		socket.emit('numberGen', core.problem());
+		socket.emit('savedUser', User.setUser(user));
+		socket.emit('numberGen', Core.problem());
 		console.log('LOG::APP::saveUser', user);
 	});
 	socket.on('removeUser', function(user) {
-		socket.emit('removedUser', core.removeUserById(user.user.id));
+		socket.emit('removedUser', User.removeUserById(user.user.id));
 		console.log('LOG::APP::removeUser', user);
 	});
 	socket.on('getUserById', function(user) {
-		socket.emit('reqUser', core.getUserById(user.user.id));
+		socket.emit('reqUser', User.getUserById(user.user.id));
 		console.log('LOG::APP::getUserById', user);	
 	});
 	socket.on('getUsers', function() {
-		socket.emit('resUsers', core.getUsers());
+		socket.emit('resUsers', User.getUsers());
 		console.log('LOG::APP::getUsers');
 	});
 });
